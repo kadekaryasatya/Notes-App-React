3c5ce12af0b1038e6ba47672ef3bc41426cbd11e
dekarya123 <arya.satyad123@gmail.com>
2022-11-07 20:44:30 +0800

    Use CustomHooks

--- src/pages/AddPage.jsx
+++ src/pages/AddPage.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import { addNote } from "../utils/api";
-import NotesInput from "../components/notes/NotesInput";
 import { useNavigate } from "react-router-dom";
+import useInput from "../hooks/useInput";
 
-function AddPage() {
+function AddPage1() {
+  const [title, onTitleChange] = useInput("");
+  const [body, onBodyChange] = useInput("");
+  const limit = 50;
+
   const navigate = useNavigate();
 
-  async function onAddNotesHandler(note) {
-    await addNote(note);
-    navigate("/");
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    /**
+     * Addnote
+     */
+    addNote({ title, body }).then((res) => {
+      if (!res.error) {
+        navigate("/");
+      }
+    });
+  };
 
   return (
-    <section>
-      <NotesInput addNotes={onAddNotesHandler} />
-    </section>
+    <div className="form-input" id="form-input">
+      <h2>Add Note</h2>
+      <form className="note-input" onSubmit={handleSubmit}>
+        <input className="note-input__title" type="text" placeholder="Add Title ..." value={title} id="title" onChange={onTitleChange} required />
+        <p className="note-input__title__char-limit">{limit - title.length} remaining character</p>
+        <textarea placeholder="Take a Note ...." value={body} onChange={onBodyChange} required />
+        <button type="submit">Add</button>
+      </form>
+    </div>
   );
 }
 
-export default AddPage;
+export default AddPage1;
