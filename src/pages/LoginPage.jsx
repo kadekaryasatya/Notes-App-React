6c163f4784ccffff2bd4ada54637dfafdcedd033
dekarya123 <arya.satyad123@gmail.com>
2022-11-09 15:48:55 +0800

    Change to functional component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import LocaleContext from "../contexts/LocaleContext";
+import useInput from "../hooks/useInput";
 import { Link } from "react-router-dom";
-import LoginInput from "../components/auth/LoginInput";
 import { login } from "../utils/api";
 
 function LoginPage({ loginSuccess }) {
-  async function onLogin({ email, password }) {
-    const { error, data } = await login({ email, password });
+  const [email, onEmailChange] = useInput("");
+  const [password, onPasswordChange] = useInput("");
 
-    if (!error) {
-      loginSuccess(data);
+  const { locale } = useContext(LocaleContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await login({ email, password });
+    if (!res.error) {
+      loginSuccess(res.data);
     }
-  }
+  };
 
   return (
     <section className="login-page">
-      <h2> Sign into your account ...</h2>
-      <LoginInput login={onLogin} />
+      <h2>{locale === "id" ? "Masuk ke Akun Anda ..." : "Sign into your account ..."}</h2>
+      <form onSubmit={handleSubmit} className="login-input">
+        <input type="email" placeholder="Email" value={email} onChange={onEmailChange} className="login-email" />
+        <input type="password" placeholder="Password" value={password} onChange={onPasswordChange} />
+        <button>{locale === "id" ? "Masuk" : "Login"}</button>
+      </form>
       <p className="register">
-        Don't have account? <Link to="/register">Register</Link>
+        {locale === "id" ? "Tidak punya akun? " : "Don't have account? "}
+        <Link to="/register">{locale === "id" ? "Daftar" : "Register"}</Link>
       </p>
     </section>
   );
 }
 
 LoginPage.propTypes = {
-  loginSuccess: PropTypes.func.isRequired,
+  loginSuccess: PropTypes.func,
 };
 
 export default LoginPage;
